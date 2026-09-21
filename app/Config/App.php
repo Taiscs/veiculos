c5810074e829573cb67019a9c30dee354491973d
Taís campos dos Santos <88733561+Taiscs@users.noreply.github.com>
2026-09-21 15:42:18 -0300

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-  public string $baseURL = 'https://veiculos-17ud.onrender.com/';
+  public string $baseURL = 'https://veiculos-l7ud.onrender.com/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
